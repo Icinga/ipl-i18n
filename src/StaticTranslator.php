5d66b9f1e5788e9452308c8ae99d54c36fd6b3b4
Eric Lippmann <eric.lippmann@icinga.com>
2020-07-14 12:34:08 +0200

    Add class StaticTranslator

---
+++ src/StaticTranslator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ipl\I18n;
+
+use ipl\Stdlib\Translator;
+
+/**
+ * Static entrypoint for a translator instance
+ */
+class StaticTranslator
+{
+    /** @var Translator */
+    public static $instance;
+}
